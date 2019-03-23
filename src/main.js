925624fe5b15a91f86a978a692ee0b488fa4040a
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-03-23 17:36:19 -0500

    pintado de imagenes y nombres de los campeones

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-/* Manejo del DOM */
+const containerChampions= document.getElementById("container-champions");
+let x = Object.values(LOL.data);
+// console.log(x)
+ for(let i=0; i<x.length;i++){
+ console.log(x[i].name)
+  containerChampions.innerHTML += `
+  <div class="img-champions"><img src="${x[i].img}"></img></div>
+    <div class="name-champions"><h5> ${x[i].name}</h5></div>
+  `
+ }
