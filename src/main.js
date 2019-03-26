2fbc852f95728ec69bdd216d772f90cd16c0bd18
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-03-26 09:13:21 -0500

    actualizando el readme

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ let x = Object.values(LOL.data);
  for(let i=0; i<x.length;i++){
  console.log(x[i].name)
   containerChampions.innerHTML += `
-  <div class="img-champions"><img src="${x[i].img}"></img></div>
-    <div class="name-champions"><h5> ${x[i].name}</h5></div>
+  <div class="row">
+    <div id="img-champions">
+      <img src="${x[i].img}">
+      </img>
+    </div>
+    <div id="name-champions">
+      <h5> ${x[i].name}</h5>
+    </div>
+  </div>
   `
- }
+}
