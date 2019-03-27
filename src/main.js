37655443ed917075fe977887989c547fcb548a4c
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-03-26 23:41:07 -0500

    modificacion de la pagina, con respecto al prototipado

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ const containerChampions= document.getElementById("container-champions");
 let x = Object.values(LOL.data);
  for(let i=0; i<x.length;i++){
   containerChampions.innerHTML += `
-  <div class="img-champions">
-  <img class="img-peque" src="${x[i].img}"></img><br>
-    <span> ${x[i].name}</span>
+  <div id="img-champions" class="row six-columna">
+    <div class="col-xs-12 col-sm-6 col-md-2">
+      <img id="img-peque" src="${x[i].img}">
+      </img>
     </div>
+      <br>
+    <div class="col-xs-12 col-sm-6 col-md-2"> ${x[i].name}
+    </div>
+  </div>
   `}
+ document.getElementsByClassName("menu").addEvenListener("click",()=>{
+   
+ })
