5a0426918acf0f6396e516a80c271fc2477f644a
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-03-29 08:53:48 -0500

    solucionando problemas

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ const containerChampions= document.getElementById("container-champions");
 let x = Object.values(LOL.data);
  for(let i=0; i<x.length;i++){
   containerChampions.innerHTML += `
-  <div id="img-champions" class="row six-columna">
-    <div class="col-xs-12 col-sm-6 col-md-2">
+  <div class="img-champions" >
       <img id="img-peque" src="${x[i].img}">
       </img>
-    </div>
       <br>
-    <div class="col-xs-12 col-sm-6 col-md-2"> ${x[i].name}
+    <div>
+     ${x[i].name}
     </div>
   </div>
   `}
  document.getElementsByClassName("menu").addEvenListener("click",()=>{
+
    
  })
