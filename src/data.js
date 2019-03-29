3e296b7f1f4065eca6d2351525f0c63a8fb9c62a
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-03-29 12:40:51 -0500

    agregando comentario en sortData

--- src/data.js
+++ src/data.js
@@ -1,27 +1,30 @@
 /* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
 const example = () => {
   return 'example';
 };
 
 const filterData=(data,condition)=>{
   let newChampions=[];
-    //  console.log(data,condition);
      for(let i=0;data.length>i;i++){
       for(let j=0;data[i].tags.length>j;j++){
           if(data[i].tags[j] === condition){
               newChampions.push(data[i]);
           }
-
       }
-
      }
-
-     return newChampions;
-    
+     return newChampions;   
 }
+// const sortData=(data,sortBy)=>{
+//   let orderChampions= [];
+//   for(let i=0;data.length>i;i++){
+//     for(let j=0;data[i].id.length>j;j++){
+//         if(data[i].id[j] === sortBy){
+//             orderChampions.push(data[i]);
+//         }
+//     }
+//     return orderChampions.sort().reverse();
+// }
+
 
 window.filterData= filterData;
+// window.sortData= sortData;
