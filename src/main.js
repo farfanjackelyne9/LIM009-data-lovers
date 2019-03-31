ef25f086c0ef41c38badeeff2a63452fa3a3d8f4
Nicole Villalobos <aleidavillalobos111@gmail.com>
2019-03-31 17:37:35 -0500

    haciendo cambios a el filterData, agregando sorData y computeStats

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 // const LOL =document.getElementById("LOL")
-
+let championsActual; //variable super global
+let tags="All";
 const containerChampions= document.getElementById("container-champions");
-let champions = Object.values(LOL.data);
+const elementoPromedio=document.getElementById("resultadoPromedio");
 
+let champions = Object.values(LOL.data);
+let promedio=computeStats(champions);
+elementoPromedio.innerText=` El Promedio de HP es de  ${promedio} `;
+
+championsActual=champions;
 const paintChampions=(championsx)=>{
   console.log(championsx.length);
   let htmlx="";
@@ -27,15 +33,41 @@ paintChampions(champions);
 console.log(k);
  for(let i=0;element.length>i;i++){
 
-  element[i].addEventListener("click",(e)=>{
-    console.log("data Capturada" , e.target.dataset.name);
-   let newChampions=filterData(champions,e.target.dataset.name);
+  element[i].addEventListener("click",()=>{
+     let elementoActual = element[i];
+    console.log("data Capturada" ,elementoActual);
+   let newChampions=filterData(champions,elementoActual.dataset.name);
    console.log(newChampions);
+   let promedio=computeStats(newChampions);
+   elementoPromedio.innerText=` El Promedio de HP es de  ${promedio} `;
+   tags=elementoActual.dataset.name;
+   championsActual=newChampions;
    paintChampions(newChampions);
   })
 
  }
 
+ const ordering= document.getElementById("order");
+ ordering.addEventListener("change", ()=>{
+   console.log(ordering.value);
+   let oValue=ordering.value;
+
+   if(oValue == "1"){
+ 
+    let newAZ= sortData(champions,tags,oValue);
+
+   paintChampions(newAZ);
+
+   }
+   else if(oValue=="2"){
+    console.log("Ordenar Z-A");
+    console.log(championsActual);
+    
+   let newZA= sortData(champions,tags,oValue);
+console.log(newZA);
+paintChampions(newZA);
+   }
+ })
  /***
   * el evento 
   * guardar la informacion en localstrogare
