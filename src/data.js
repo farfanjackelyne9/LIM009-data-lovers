7d8ffcfb336ef9616863a38d761747488eae3b1f
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-04-11 08:43:44 -0500

    arreglando codigo(eliminando espacios innecesarios)

--- src/data.js
+++ src/data.js
@@ -4,8 +4,8 @@ const filterData = (data, condition) => {
   let newChampions = [];
   for (let i = 0; data.length > i; i++) { /* para recorrer los campeones*/
     for (let j = 0; data[i].tags.length > j; j++) { /* para recorrar los tags y validar si hacen math con las condicion*/
-      if (data[i].tags[j] === condition) { //validamos unos de los tantos roles que puede tener el champion  , con la condition
-        newChampions.push(data[i]);  //agregamos a ese campion a otro array nuevo
+      if (data[i].tags[j] === condition) { /* validamos unos de los tantos roles que puede tener el champion  , con la condition*/
+        newChampions.push(data[i]); /* agregamos a ese campion a otro array nuevo*/
       }
     }
   }
@@ -23,28 +23,25 @@ const sortData = (data, sortBy, sortOrder) => {
   }
 
 
-  if (sortOrder === "1") { //ordenar de la A-Z
+  if (sortOrder === "1") { /* ordenar de la A-Z*/
     let newAZ = championFilter.sort((a, b) => {
       return a.name.localeCompare(b.name);
     });
     return newAZ;
-  } 
-  else  { //ordenar de Z-A
+  } else { /* ordenar de Z-A*/
     let newZA = championFilter.sort((a, b) => {
       return b.name.localeCompare(a.name);
     });
     return newZA;
   }
- 
-
 };
 
 const computeStats = (data) => {
   /* (15+25+35)/3*/
   let statsName = "hp";
   /* [{},{},{}]*/
-  let getStats = data.map((champion) => { 
-    return champion.stats[statsName]; 
+  let getStats = data.map((champion) => {
+    return champion.stats[statsName];
   }); // filtra y te devulve en array la propiedad que especificas en el return
   let sumatory = 0;
   for (let i = 0; getStats.length > i; i++) {
